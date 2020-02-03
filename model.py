2914cf7cdb805a75b5dc450149e6cfa0eb99c687
Ale Battisti <alebat25@yahoo.it>
2020-02-03 22:35:56 +0100

    Added models, workong on P3D extraction

--- model.py
+++ model.py
@@ -25,24 +25,25 @@ class selfAttn(nn.Module):
 
 
 class conv_lstm(nn.Module):
-    def __init__(self, hidden_size, kernel, stride, nb_filter, input_size):
+    def __init__(self, hidden_size, kernel, stride, nb_filter, input_size, model='skip_lstm'):
         super(conv_lstm, self).__init__()
         self.conv = nn.Sequential(nn.Conv1d(input_size, nb_filter, kernel, stride),
                                   nn.ReLU(),
                                   nn.BatchNorm1d(nb_filter)
                                   )
-        self.lstm = create_model(model='skip_lstm',
+        self.lstm = create_model(model=model,
                                  input_size=nb_filter,
                                  hidden_size=hidden_size,
                                  num_layers=1)
         # self.lstm = lstm_cell(input_size=nb_filter, hidden_size=hidden_size, batch_first=True, layer_norm=True)
         self.hidden_size = hidden_size
+        self.model_name = model
 
     def forward(self, input):
         input = self.conv(input.permute(0, 2, 1))
         input = input.permute(0, 2, 1)
         output = self.lstm(input)
-        output, hx, updated_state = split_rnn_outputs('skip_lstm', output)
+        output, hx, updated_state = split_rnn_outputs(self.model_name, output)
         return output[:, -1, :]
 
 
@@ -51,6 +52,14 @@ def get_scoring_model(name):
         return Scoring(4096)
     elif name == 'scoring-dropout.5':
         return ScoringDropout(4096, dropout_p=0.5)
+    elif name == 'scoring-dropout.7':
+        return ScoringDropout(4096, dropout_p=0.7)
+    elif name == 'scoring-dropout.7-multionly':
+        return ScoringDropoutMultiOnly(4096, dropout_p=0.7)
+    elif name == 'scoring-dropout.7-atteonly':
+        return ScoringDropoutAtteOnly(4096, dropout_p=0.7)
+    elif name == 'scoring-dropout.5-monogruonly':
+        return ScoringDropoutMonoGruOnly(4096, dropout_p=0.5)
 
     raise NameError(f'Model {name} not implemented.')
 
@@ -181,9 +190,9 @@ class ScoringDropout(nn.Module):
         return regr_loss_fn(regression, actuals)
 
 
-class ScoringMultiOnly(nn.Module):
-    def __init__(self, feature_size):
-        super(ScoringMultiOnly, self).__init__()
+class ScoringDropoutMultiOnly(nn.Module):
+    def __init__(self, feature_size, dropout_p=0.5):
+        super(ScoringDropoutMultiOnly, self).__init__()
 
         conv_input = 128
         self.conv = nn.Sequential(
@@ -195,37 +204,70 @@ class ScoringMultiOnly(nn.Module):
         self.scale1 = conv_lstm(hidden_size, 2, 1, 256, conv_input)
         self.scale2 = conv_lstm(hidden_size, 4, 2, 256, conv_input)
         self.scale3 = conv_lstm(hidden_size, 8, 4, 256, conv_input)
-        self.attn = selfAttn(conv_input, 64, 50)
-        self.lstm = nn.LSTM(input_size=conv_input, hidden_size=hidden_size, num_layers=1, batch_first=True)
         self.linear_skip1 = nn.Linear(hidden_size, 64)
         self.linear_skip2 = nn.Linear(hidden_size, 64)
         self.linear_skip3 = nn.Linear(hidden_size, 64)
         self.linear_merge = nn.Linear(64 * 3, 64)
         self.cls = nn.Linear(64, 1)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.7)
+        self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, model_input):
+        model_input = model_input.permute(0, 2, 1)
+        model_input = self.conv.forward(model_input).permute(0, 2, 1)
+        output = torch.cat([
+            self.dropout(self.relu(
+                self.linear_skip1(
+                    self.scale1(model_input)
+                ))),
+            self.dropout(self.relu(
+                self.linear_skip2(
+                    self.scale2(model_input)
+                ))),
+            self.dropout(self.relu(
+                self.linear_skip3(
+                    self.scale3(model_input)
+                )))
+        ], 1)
+        output = self.dropout(self.relu(self.linear_merge(output)))
+        return self.cls(output), None
+
+    def loss(self, regression, actuals):
+        """
+        use mean square error for regression and cross entropy for classification
+        """
+        regr_loss_fn = nn.MSELoss()
+        return regr_loss_fn(regression, actuals)
+
+
+class ScoringDropoutAtteOnly(nn.Module):
+    def __init__(self, feature_size, dropout_p=0.5):
+        super(ScoringDropoutAtteOnly, self).__init__()
+
+        conv_input = 128
+        self.conv = nn.Sequential(
+            nn.Conv1d(feature_size, conv_input, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm1d(conv_input)
+        )
+        hidden_size = 256
+        self.attn = selfAttn(conv_input, 64, 50)
+        self.lstm = nn.LSTM(input_size=conv_input, hidden_size=hidden_size, num_layers=1, batch_first=True)
+        self.linear_attn = nn.Linear(hidden_size, 64)
+        self.linear_merge = nn.Linear(64, 64)
+        self.cls = nn.Linear(64, 1)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, model_input):
         model_input = model_input.permute(0, 2, 1)
         model_input = self.conv.forward(model_input).permute(0, 2, 1)
         attn, penal = self.attn.forward(model_input)
         attn, _ = self.lstm(attn)
-        m_output = torch.cat([
-            self.relu(
-                self.linear_skip1(
-                    self.scale1(model_input)
-                )),
-            self.relu(
-                self.linear_skip2(
-                    self.scale2(model_input)
-                )),
-            self.relu(
-                self.linear_skip3(
-                    self.scale3(model_input)
-                ))
-        ], 1)
-        output = m_output
-        output = self.relu(self.linear_merge(output))
+        attn = attn[:, -1, :]
+        output = self.dropout(self.relu(self.linear_attn(attn)))
+        output = self.dropout(self.relu(self.linear_merge(output)))
         return self.cls(output), penal
 
     def loss(self, regression, actuals):
@@ -236,9 +278,9 @@ class ScoringMultiOnly(nn.Module):
         return regr_loss_fn(regression, actuals)
 
 
-class ScoringAttOnly(nn.Module):
-    def __init__(self, feature_size):
-        super(ScoringAttOnly, self).__init__()
+class ScoringDropoutMonoGruOnly(nn.Module):
+    def __init__(self, feature_size, dropout_p=0.5):
+        super(ScoringDropoutMonoGruOnly, self).__init__()
 
         conv_input = 128
         self.conv = nn.Sequential(
@@ -247,24 +289,20 @@ class ScoringAttOnly(nn.Module):
             nn.BatchNorm1d(conv_input)
         )
         hidden_size = 256
-        self.scale1 = conv_lstm(hidden_size, 2, 1, 256, conv_input)
-        self.scale2 = conv_lstm(hidden_size, 4, 2, 256, conv_input)
-        self.scale3 = conv_lstm(hidden_size, 8, 4, 256, conv_input)
-        self.attn = selfAttn(conv_input, 64, 50)
-        self.lstm = nn.LSTM(input_size=conv_input, hidden_size=hidden_size, num_layers=1, batch_first=True)
-        self.linear_attn = nn.Linear(hidden_size, 64)
+        self.scale2 = conv_lstm(hidden_size, 4, 2, 256, conv_input, model='nn_gru')
+        self.linear_skip2 = nn.Linear(hidden_size, 64)
+        self.linear_merge = nn.Linear(64, 64)
         self.cls = nn.Linear(64, 1)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.7)
+        self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, model_input):
         model_input = model_input.permute(0, 2, 1)
         model_input = self.conv.forward(model_input).permute(0, 2, 1)
-        attn, penal = self.attn.forward(model_input)
-        attn, _ = self.lstm(attn)
-        attn = attn[:, -1, :]
-        output = self.relu(self.linear_attn(attn))
-        return self.cls(output), penal
+        output = self.dropout(self.relu(self.linear_skip2(self.scale2(model_input))))
+        output = self.dropout(self.relu(self.linear_merge(output)))
+        return self.cls(output), None
 
     def loss(self, regression, actuals):
         """
